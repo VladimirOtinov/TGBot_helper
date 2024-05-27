37685bbaff5511d9a03d48a929ad59a0d9169b0f
Vladimir <25050otinov@gmail.com>
2024-05-27 13:46:38 +0300

    Changed structure of message

--- Bot_helper/main.py
+++ Bot_helper/main.py
@@ -1,15 +1,13 @@
 import json
 import logging
 import asyncio
+import aiosmtplib
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiosmtplib import send
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from config import TOKEN, SMTP_SERVER, SMTP_PORT, SUPPORT_EMAIL, BOT_EMAIL, EMAIL_PASSWORD
 
 # Настройка бота и диспетчера
@@ -17,43 +15,46 @@ bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
+
 # Определение класса состояния
 class Form(StatesGroup):
     waiting_for_message = State()
     waiting_for_send = State()
 
+
 # Обработчик команды /start
 @dp.message(CommandStart())
 async def handle_start(message: Message, state: FSMContext):
     args = message.text.split()
+    await state.update_data(user_id=message.from_user.id)
     if len(args) > 1:
         deep_link_id = args[1]
         await state.update_data(deep_link_id=deep_link_id)
-        await message.answer("Добрый день! Я бот для передачи информации в тех. поддержку. "
-                             "Опишите вашу ошибку в _одном сообщении_, я передам ее нашим специалистам.",
-                             parse_mode="Markdown")
+        await message.answer(
+            "Добрый день! Я бот для передачи информации в тех. поддержку. "
+            "Опишите вашу ошибку в _одном сообщении_, я передам ее нашим специалистам.",
+            parse_mode="Markdown"
+        )
         await state.set_state(Form.waiting_for_message)
     else:
         await message.answer("Добрый день")
 
+
 # Обработчик обычных сообщений
 @dp.message(Form.waiting_for_message)
 async def handle_message(message: Message, state: FSMContext):
-    state_data = await state.get_data()
-    deep_link_id = state_data.get("deep_link_id")
-
-    # Сохранение данных в состоянии
     await state.update_data(user_message=message.text)
     await message.answer("Ваше сообщение было сохранено. Для отправки нажмите или введите /send.")
     await state.set_state(Form.waiting_for_send)
+
 
 # Обработчик команды /send
 @dp.message(Command(commands=["send"]))
 async def handle_send(message: Message, state: FSMContext):
     state_data = await state.get_data()
-    user_id = message.from_user.id
-    user_message = state_data.get("user_message")
+    user_id = state_data.get("user_id")
     deep_link_id = state_data.get("deep_link_id")
+    user_message = state_data.get("user_message")
 
     if user_message:
         # Формирование данных для отправки по почте
@@ -61,21 +62,26 @@ async def handle_send(message: Message, state: FSMContext):
             'user_id': user_id,
             'message': user_message
         }
-        email_content = json.dumps(email_data, indent=4)
-        await send_email(email_content, deep_link_id)
+        email_content = json.dumps(email_data, indent=4, ensure_ascii=False)
+        subject = f"param: {deep_link_id} uid: {user_id}"
+        await send_email(email_content, subject)
         await message.answer("Ваше обращение было передано в тех. поддержку")
         await state.clear()
     else:
-        await message.answer("Сообщение не найдено. Пожалуйста, опишите вашу ошибку еще раз.")
+        await message.answer("Что-то пошло не так. Пожалуйста, опишите вашу ошибку еще раз.")
 
-async def send_email(content: str, deep_link_id: int):
+
+async def send_email(content: str, subject: str):
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
     msg = MIMEMultipart()
     msg['From'] = BOT_EMAIL
     msg['To'] = SUPPORT_EMAIL
-    msg['Subject'] = f"{deep_link_id}"
+    msg['Subject'] = subject
     msg.attach(MIMEText(content, 'plain'))
 
-    await send(
+    await aiosmtplib.send(
         msg,
         hostname=SMTP_SERVER,
         port=SMTP_PORT,
@@ -84,8 +90,10 @@ async def send_email(content: str, deep_link_id: int):
         use_tls=True
     )
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
