70a61bbe10e96641b76db682f93d9984b527faa4
Vladimir <25050otinov@gmail.com>
2024-06-07 18:49:48 +0300

    Add new data and change README file

--- Bot_helper/MessageStruct.py
+++ Bot_helper/MessageStruct.py
@@ -1,10 +1,15 @@
 class MessageStruct:
-    def __init__(self, text_message, uid, time_send, deep_link, chat_id):
+    """
+    Класс определяющий структуру сообщения которое отправляется в тех. поддержку
+    """
+    def __init__(self, text_message, uid, time_send, deep_link, chat_id, photo=None, video=None):
         self.deep_link = deep_link
         self.time_send = time_send
         self.uid = uid
         self.text_message = text_message
         self.chat_id = chat_id
+        self.photo = photo
+        self.video = video
 
     def to_json(self):
         return {
@@ -12,5 +17,7 @@ class MessageStruct:
             "deep_link": f"{self.deep_link}",
             "chat_id": f"{self.chat_id}",
             "time_send": f"{self.time_send}",
-            "text_message": f"{self.text_message}"
-            }
+            "text_message": f"{self.text_message}",
+            "photo": self.photo,
+            "video": self.video
+        }
